0fd9379a2205b4709ab111901d4f23f153f3dadb
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-18 23:58:18 +0600

    added templating and bootstrap

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# dummy data
+
+posts = [
+{
+    'post_title'  : "Richard Madden",
+    'post_author' : 'Faria Tahsin',
+    'post_content': 'I love Richard Madden.',
+    'post_date'   : 'December 25th, 2019'
+},
+{
+    'post_title'  : "Guild Wars 2",
+    'post_author' : 'Knyght',
+    'post_content': 'Guild Wars 2 was an epic game, but it sort of sucks now.',
+    'post_date'   : 'January 3, 2020'
+}
+]
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
